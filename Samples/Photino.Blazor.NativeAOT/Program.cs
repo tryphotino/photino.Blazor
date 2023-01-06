7b50211e83dea280ac7b66140b1aa3d7e9b90203
Andrii Kurdiumov <kant2002@gmail.com>
2023-01-06 22:04:08 +0600

    Final polish after rename

--- Samples/Photino.Blazor.NativeAOT/Program.cs
+++ Samples/Photino.Blazor.NativeAOT/Program.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Photino.Blazor;
 
-namespace Photino.Blazor.Sample
+namespace Photino.Blazor.NativeAOT
 {
     class Program
     {
